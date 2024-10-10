6386d923b93877f486bee8dc0ee30dfc40557994
sisam1234 <sisamghaju72@gmail.com>
2024-10-10 18:57:05 +0545

    add view and edit

--- src/Notepad.java
+++ src/Notepad.java
@@ -2,56 +2,69 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 import java.io.*;
-import java.awt.datatransfer.*;
-public class Notepad extends JFrame implements ActionListener{
-        JFrame f;
-        JTextArea a;
-        File file;
-        Notepad(){
-         f = new JFrame();
+
+public class Notepad extends JFrame implements ActionListener, ItemListener {
+    JFrame f;
+    JTextArea a;
+    File file;
+    int fontSize = 20;
+    JCheckBox wordwrap;
+
+    Notepad() {
+        f = new JFrame();
         setTitle("Notepad");
         JMenuBar jb = new JMenuBar();
-        JMenu file =  new JMenu("File");
+        JMenu file = new JMenu("File");
         jb.add(file);
-        JMenuItem newdoc,open,save;
+        JMenuItem newdoc, open, save;
         newdoc = new JMenuItem("New");
-        newdoc.setAccelerator( KeyStroke.getKeyStroke(KeyEvent.VK_N, ActionEvent.CTRL_MASK));
-        save=new JMenuItem("Save");
-        save.setAccelerator( KeyStroke.getKeyStroke(KeyEvent.VK_S, ActionEvent.CTRL_MASK));
+        newdoc.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_N, ActionEvent.CTRL_MASK));
+        save = new JMenuItem("Save");
+        save.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_S, ActionEvent.CTRL_MASK));
         open = new JMenuItem("Open");
-        open.setAccelerator( KeyStroke.getKeyStroke(KeyEvent.VK_O, ActionEvent.CTRL_MASK));
+        open.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_O, ActionEvent.CTRL_MASK));
         file.add(newdoc);
         file.add(save);
         file.add(open);
         newdoc.addActionListener(this);
         save.addActionListener(this);
         open.addActionListener(this);
-        JMenu edit =  new JMenu("Edit");
+        JMenu edit = new JMenu("Edit");
         jb.add(edit);
-        JMenuItem copy,paste,cut,selectall;
+        JMenuItem copy, paste, cut, selectall;
         copy = new JMenuItem("Copy");
-        copy.setAccelerator( KeyStroke.getKeyStroke(KeyEvent.VK_C, ActionEvent.CTRL_MASK));
+        copy.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_C, ActionEvent.CTRL_MASK));
         paste = new JMenuItem("Paste");
-        paste.setAccelerator( KeyStroke.getKeyStroke(KeyEvent.VK_P, ActionEvent.CTRL_MASK));
-        cut =  new JMenuItem("Cut");
-        cut.setAccelerator( KeyStroke.getKeyStroke(KeyEvent.VK_X, ActionEvent.CTRL_MASK));
-        selectall =new JMenuItem("Select All");
-        selectall.setAccelerator( KeyStroke.getKeyStroke(KeyEvent.VK_A, ActionEvent.CTRL_MASK));
-        edit.add(copy);edit.add(paste);edit.add(cut);edit.add(selectall);
-        JMenu view =  new JMenu("View");
+        paste.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_P, ActionEvent.CTRL_MASK));
+        cut = new JMenuItem("Cut");
+        cut.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_X, ActionEvent.CTRL_MASK));
+        selectall = new JMenuItem("Select All");
+        selectall.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_A, ActionEvent.CTRL_MASK));
+        edit.add(copy);
+        edit.add(paste);
+        edit.add(cut);
+        edit.add(selectall);
+        copy.addActionListener(this);
+        paste.addActionListener(this);
+        cut.addActionListener(this);
+        selectall.addActionListener(this);
+        JMenu view = new JMenu("View");
         jb.add(view);
-        JMenuItem wordwrap,zoomin,zoomout;
+        JMenuItem zoomin, zoomout;
         JMenu zoom = new JMenu("Zoom");
-        zoomin =new JMenuItem("Zoom in");
+        zoomin = new JMenuItem("Zoom in");
         zoomout = new JMenuItem("Zoom out");
-        wordwrap = new JMenuItem("Word wrap");
+        wordwrap = new JCheckBox("Wordwrap");
         view.add(zoom);
         zoom.add(zoomin);
         zoom.add(zoomout);
         view.add(wordwrap);
+        zoomin.addActionListener(this);
+        zoomout.addActionListener(this);
+        wordwrap.addItemListener(this);
         setJMenuBar(jb);
-         a = new JTextArea();
-        a.setFont(new Font("SAN_SERIF",Font.PLAIN,20));
+        a = new JTextArea();
+        a.setFont(new Font("SAN_SERIF", Font.PLAIN, fontSize));
         add(a);
         JScrollPane pane = new JScrollPane(a);
         pane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
@@ -63,20 +76,20 @@ public class Notepad extends JFrame implements ActionListener{
         setVisible(true);
 
     }
+
     public void SaveFile(String filePath) throws IOException {
         BufferedWriter writer = new BufferedWriter(new FileWriter(filePath));
         writer.write(a.getText()); // Write the content of JTextArea to the file
         writer.close(); // Close the writer
     }
-    public void actionPerformed(ActionEvent e){
+
+    public void actionPerformed(ActionEvent e) {
         JFileChooser jc = new JFileChooser();
-        if(e.getActionCommand().equals("New")){ 
-			//new
-			this.setTitle("Untitled.txt - Notepad");
-			a.setText("");
-			f=null;
-		}
-        else if(e.getActionCommand().equals("Open")){
+        if (e.getActionCommand().equals("New")) {
+            this.setTitle("Untitled.txt - Notepad");
+            a.setText("");
+            f = null;
+        } else if (e.getActionCommand().equals("Open")) {
             int returnValue = jc.showOpenDialog(this); // Open dialog
 
             if (returnValue == JFileChooser.APPROVE_OPTION) {
@@ -86,15 +99,14 @@ public class Notepad extends JFrame implements ActionListener{
                     BufferedReader reader = new BufferedReader(new FileReader(selectedFile));
                     a.read(reader, null); // Load the file's content into the JTextArea
                     reader.close();
-    
+
                     // Set the title of the window to the name of the opened file
                     this.setTitle(selectedFile.getName() + " - Notepad");
                 } catch (Exception ex) {
                     ex.printStackTrace();
                 }
             }
-        }
-        else if(e.getActionCommand().equals("Save")){
+        } else if (e.getActionCommand().equals("Save")) {
             if (file != null) {
                 // Set current directory if a file is already opened
                 jc.setCurrentDirectory(file); // Use the parent directory of the current file
@@ -104,21 +116,47 @@ public class Notepad extends JFrame implements ActionListener{
                 jc.setSelectedFile(new File("Untitled.txt"));
             }
 
-            int ret=jc.showSaveDialog(null);
-				
-			if(ret == JFileChooser.APPROVE_OPTION){
-				try{
-					
-					File fyl=jc.getSelectedFile();
-					SaveFile(fyl.getAbsolutePath());
-					this.setTitle(fyl.getName()+ " - Notepad");
-					file=fyl;
-					
-				}catch(Exception ers2){}
-			}
+            int ret = jc.showSaveDialog(null);
+
+            if (ret == JFileChooser.APPROVE_OPTION) {
+                try {
+
+                    File fyl = jc.getSelectedFile();
+                    SaveFile(fyl.getAbsolutePath());
+                    this.setTitle(fyl.getName() + " - Notepad");
+                    file = fyl;
+
+                } catch (Exception ers2) {
+                }
+            }
+        } else if (e.getActionCommand().equals("Copy")) {
+            a.copy();
+        } else if (e.getActionCommand().equals("Paste")) {
+            a.paste();
+        } else if (e.getActionCommand().equals("Select All")) {
+            a.selectAll();
+        } else if (e.getActionCommand().equals("Cut")) {
+            a.cut();
+        } else if (e.getActionCommand().equals("Zoom in")) {
+            fontSize += 4; // Increase the font size by 2
+            a.setFont(new Font("SAN_SERIF", Font.PLAIN, fontSize));
+        } else if (e.getActionCommand().equals("Zoom out")) {
+            fontSize -= 4; // Increase the font size by 2
+            a.setFont(new Font("SAN_SERIF", Font.PLAIN, fontSize));
         }
     }
-        public static void main(String[] args) {
-            new Notepad();
+
+    public void itemStateChanged(ItemEvent e) {
+        if (e.getSource() == wordwrap) {
+            if (wordwrap.isSelected()) {
+                a.setLineWrap(true);
+            } else {
+                a.setLineWrap(false);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        new Notepad();
     }
 }
